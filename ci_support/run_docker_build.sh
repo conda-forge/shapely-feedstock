90697975d3ab6ff80777af75596c35444e021429
ocefpaf <ocefpaf@gmail.com>
2017-01-05 15:08:05 -0300

    np110py36

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -14,7 +14,7 @@ config=$(cat <<CONDARC
 
 channels:
  - conda-forge
- - defaults # As we need conda-build
+ - defaults
 
 conda-build:
  root-dir: /feedstock_root/build_artefacts
@@ -25,8 +25,8 @@ CONDARC
 )
 
 cat << EOF | docker run -i \
-                        -v ${RECIPE_ROOT}:/recipe_root \
-                        -v ${FEEDSTOCK_ROOT}:/feedstock_root \
+                        -v "${RECIPE_ROOT}":/recipe_root \
+                        -v "${FEEDSTOCK_ROOT}":/feedstock_root \
                         -a stdin -a stdout -a stderr \
                         condaforge/linux-anvil \
                         bash || exit $?
@@ -45,7 +45,7 @@ source run_conda_forge_build_setup
 conda install --yes --quiet conda-build=2
 conda info
 
-# Embarking on 7 case(s).
+# Embarking on 8 case(s).
     set -x
     export CONDA_NPY=110
     export CONDA_PY=27
@@ -84,6 +84,13 @@ conda info
     set -x
     export CONDA_NPY=111
     export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
+    export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
