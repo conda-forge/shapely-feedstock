9be3dd4cef46d283c8983a7403b30bbd2f0f4f45
Filipe Fernandes <ocefpaf@gmail.com>
2019-06-22 19:45:53 -0300

    speedups on win

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-export GEOS_DIR=$PREFIX
+rm -rf shapely/speedups/_speedups.c
+rm -rf shapely/vectorized/_vectorized.c
 
-$PYTHON -m pip install --no-deps --ignore-installed .
+export GEOS_DIR=${PREFIX}
 
+# Workaround weird setup.py logic that only triggers force_cython if the MANIFEST.in file is present.
+touch MANIFEST.in
+
+${PYTHON} -m pip install --no-deps --ignore-installed .
