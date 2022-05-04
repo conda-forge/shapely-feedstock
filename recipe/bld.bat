394b3c62a3c17273275ca2e45dfcfd0b06deec11
akrherz <akrherz@iastate.edu>
2022-05-04 11:33:22 -0500

    tweak windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,11 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
-set GEOS_LIBRARY_PATH=%LIBRARY_BIN%\geos_c.dll
-
-del /f shapely\speedups\_speedups.c
-del /f shapely\vectorized\_vectorized.c
+set GEOS_LIBRARY_PATH=%LIBRARY_LIB%
 
 "%PYTHON%" -m cython shapely\speedups\_speedups.pyx
 "%PYTHON%" -m cython shapely\vectorized\_vectorized.pyx
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed --verbose . ^
-                          --global-option=build_ext ^
+                          --global-option="build_ext" ^
                           --global-option="-I%LIBRARY_INC%" ^
                           --global-option="-L%LIBRARY_LIB%" ^
                           --global-option="-lgeos_c"
