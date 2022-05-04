559fc38ad4252c827e68e5c132902c4f81c1bf3d
akrherz <akrherz@iastate.edu>
2022-05-04 12:31:00 -0500

    remove un-needed cython calls

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,5 @@
 
 set GEOS_LIBRARY_PATH=%LIBRARY_LIB%
 
-"%PYTHON%" -m cython shapely\speedups\_speedups.pyx
-"%PYTHON%" -m cython shapely\vectorized\_vectorized.pyx
-
 %PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgeos_c -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
