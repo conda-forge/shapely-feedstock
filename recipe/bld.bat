58b6292f4dd2ede179d33d44b95bf1f69890b50c
akrherz <akrherz@iastate.edu>
2022-05-04 11:44:26 -0500

    don't use pip for building on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,9 +4,5 @@ set GEOS_LIBRARY_PATH=%LIBRARY_LIB%
 "%PYTHON%" -m cython shapely\speedups\_speedups.pyx
 "%PYTHON%" -m cython shapely\vectorized\_vectorized.pyx
 
-"%PYTHON%" -m pip install --no-deps --ignore-installed --verbose . ^
-                          --global-option="build_ext" ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgeos_c"
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgeos_c -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
