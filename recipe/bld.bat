e170f26da778e17d6343d92ef676c2dd3af9f68a
Mike Taves <mwtoews@gmail.com>
2022-05-06 09:59:44 +1200

    Restore to bld.bat "pip install" with "--no-use-pep517"

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,13 @@
 
-set GEOS_LIBRARY_PATH=%LIBRARY_BIN%
+:: set GEOS_LIBRARY_PATH=%LIBRARY_BIN%
 
 del /f shapely\speedups\_speedups.c
 del /f shapely\vectorized\_vectorized.c
 
-"%PYTHON%" setup.py build_ext ^
-    -I"%LIBRARY_INC%" ^
-    -lgeos_c ^
-    -L"%LIBRARY_LIB%" ^
-    install ^
-    --single-version-externally-managed ^
-    --record record.txt
+"%PYTHON%" -m pip install . -vv ^
+    --no-deps --ignore-installed --no-use-pep517 ^
+    --global-option=build_ext ^
+    --global-option="-I%LIBRARY_INC%" ^
+    --global-option="-L%LIBRARY_LIB%" ^
+    --global-option="-lgeos_c"
 if errorlevel 1 exit 1
