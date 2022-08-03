a36e8b9fac0e8f7d5caae5f453c4c06d86931d60
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-08-03 14:19:33 +0200

    update from main branch

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,13 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
-set GEOS_LIBRARY_PATH=%LIBRARY_BIN%\geos_c.dll
+:: set GEOS_LIBRARY_PATH=%LIBRARY_BIN%
 
 del /f shapely\speedups\_speedups.c
 del /f shapely\vectorized\_vectorized.c
 
-cython shapely\speedups\_speedups.pyx
-cython shapely\vectorized\_vectorized.pyx
-
-"%PYTHON%" -m pip install --no-deps --ignore-installed --verbose . ^
-                          --global-option=build_ext ^
-                          --global-option="-I%LIBRARY_INC%" ^
-                          --global-option="-L%LIBRARY_LIB%" ^
-                          --global-option="-lgeos_c"
+"%PYTHON%" -m pip install . -vv ^
+    --no-deps --ignore-installed --no-use-pep517 ^
+    --global-option=build_ext ^
+    --global-option="-I%LIBRARY_INC%" ^
+    --global-option="-L%LIBRARY_LIB%" ^
+    --global-option="-lgeos_c"
 if errorlevel 1 exit 1
--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,12 +1,16 @@
 import platform
 import py
+import os
+import sys
 
 implementation = platform.python_implementation()
 print('implementation: {}'.format(implementation))
+target_platform = os.environ["target_platform"]
+print(f'target platform: {target_platform}')
+py_version = sys.version_info[:2]
+print(f'python version: {py_version}')
 
-# SvgTestCase.test_collection is failing due to GEOS 3.9 different coordinate order
-# (it's fixed on master so can be removed again with Shapely 1.8)
-pytest_args = ['tests', '-k', 'not test_collection']
+pytest_args = ['tests']
 
 if implementation != 'PyPy':
     from shapely import speedups
@@ -24,7 +28,7 @@ py.test.cmdline.main(pytest_args)
 from shapely.geometry import LineString
 
 ls = LineString([(0, 0), (10, 0)])
-# On OSX causes an abort trap, due to https://github.com/Toblerity/Shapely/issues/177
+# On OSX causes an abort trap, due to https://github.com/shapely/shapely/issues/177
 r = ls.wkt
 area = ls.buffer(10).area
 
